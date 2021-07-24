93ff702af7630064232b8cf8d56e42cd2ad29b48
гк <si-roga@yandex.ru>
2021-07-24 20:08:41 +0300

    add comporation and filters 3 ver

--- src/by/belarusian/farmer/utils/HarvestUtil.java
+++ src/by/belarusian/farmer/utils/HarvestUtil.java
@@ -4,6 +4,7 @@ import by.belarusian.farmer.enums.Type;
 import by.belarusian.farmer.model.Harvest;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class HarvestUtil {
@@ -27,5 +28,13 @@ public class HarvestUtil {
         }
         return resultHarvest;
     }
+            List<Harvest> filterOverWeight(List<Harvest> list, Long weight){
 
+                Iterator <Harvest> iter = list.iterator();
+                while (iter.hasNext()){
+                    iter.next().getWeight();
+                        iter.remove();
+                }
+                return list;
+            }
 }
