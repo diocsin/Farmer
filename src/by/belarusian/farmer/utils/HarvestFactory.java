04036ee74121e4b6618d5790021f0e234d4a85aa
Artifact <artifact911@gmail.com>
2021-07-24 20:23:15 +0300

    Started to create generators

--- src/by/belarusian/farmer/utils/HarvestFactory.java
+++ src/by/belarusian/farmer/utils/HarvestFactory.java
@@ -2,25 +2,46 @@ package by.belarusian.farmer.utils;
 
 import by.belarusian.farmer.enums.Color;
 import by.belarusian.farmer.model.Berry;
+import by.belarusian.farmer.model.Fruit;
 import by.belarusian.farmer.model.Harvest;
+import by.belarusian.farmer.model.Vegetable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class HarvestFactory {
 
-    public static List<Harvest> getHarvest(int number) {
+    private Random rand = new Random();
+    private Color[] colors = Color.values();
 
-        Random rand = new Random();
-        Color[] colors = Color.values();
+
+    public List<Harvest> getHarvest(int number) {
+        List<Harvest> harvestList = new ArrayList<>();
+
 
         for (int i = 0; i < number; i++) {
             switch (rand.nextInt(2)) {
                 case 0:
-//                    return new Berry(rand.nextInt(20), colors[rand.nextInt(10)])
+
             }
         }
 
+
+        return harvestList;
+    }
+
+    private Berry getBerry(){
+
+        return null;
+    }
+
+    private Fruit getFruit(){
+
+        return null;
+    }
+
+    private Vegetable getVegetable(){
 
         return null;
     }
