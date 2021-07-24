8e7dbde9604aa92d23625568bde2a4403dfb9d0f
гк <Kirill>
2021-07-24 20:33:30 +0300

    Abstract classes added

--- src/by/belarusian/farmer/utils/HarvestFactory.java
+++ src/by/belarusian/farmer/utils/HarvestFactory.java
@@ -1,7 +1,33 @@
 package by.belarusian.farmer.utils;
 
+import by.belarusian.farmer.enums.Color;
+import by.belarusian.farmer.model.Fruit;
+import by.belarusian.farmer.model.Harvest;
+import by.belarusian.farmer.model.fruits.Apple;
+import by.belarusian.farmer.model.fruits.Lemon;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class HarvestFactory {
 
+    public ArrayList<Harvest> generate(Class cl, int count) {
+
+        ArrayList<Harvest> harvests = new ArrayList<>();
+        Random random = new Random();
+
+        if (cl.isInstance(Apple.class)) {
+            for(int i = 0; i < count; i++) {
+                harvests.add(new Apple(random.nextInt(3), Color.GREEN));
+            }
+
+        }
+        return harvests;
+    }
+
+    public void test() {
+        generate(Apple.class, 23);
+    }
 
 
 }
