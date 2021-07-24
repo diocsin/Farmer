db3a4a3648491882f99a6a00c14ce94951675034
Kiril Gusakov <gusakov24@gmail.com>
2021-07-24 20:27:33 +0300

    Start generator

--- src/by/belarusian/farmer/utils/HarvestFactory.java
+++ src/by/belarusian/farmer/utils/HarvestFactory.java
@@ -1,7 +1,26 @@
 package by.belarusian.farmer.utils;
 
+import by.belarusian.farmer.enums.Type;
+import by.belarusian.farmer.model.Harvest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class HarvestFactory {
 
+    Type[] types = Type.values();
+    public List<Harvest> farmGenerator(int amount, Type type) {
+
+        List<Harvest> harvestList = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+
+        }
+
+
+        return null;
+    }
 
 
 }
