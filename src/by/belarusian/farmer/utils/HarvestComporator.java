3e3f66ba19eec0228f182b3371969563c4428d41
гк <si-roga@yandex.ru>
2021-07-24 20:06:09 +0300

    add comporation and filters 2 ver

--- src/by/belarusian/farmer/utils/HarvestComporator.java
+++ src/by/belarusian/farmer/utils/HarvestComporator.java
@@ -1,10 +1,12 @@
 package by.belarusian.farmer.utils;
 
+import by.belarusian.farmer.model.Harvest;
+
 import java.util.Comparator;
 
-public class HarvestComporator implements Comparator <?> {
+public class HarvestComporator implements Comparator <Harvest> {
     @Override
-    public int compare(Object o1, Object o2) {
-        return o1.weight-o2.weight;
+    public int compare(Harvest o1, Harvest o2) {
+        return o1.getWeight()-o2.getWeight();
     }
 }
