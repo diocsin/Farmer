c3fff85c80c1ef3c75cb78314cc1c4732b5a2882
Kiril Gusakov <gusakov24@gmail.com>
2021-07-24 19:08:25 +0300

    Add final fruit's classes

--- src/by/belarusian/farmer/model/Lemon.java
+++ src/by/belarusian/farmer/model/Lemon.java
@@ -1,8 +1,22 @@
 package by.belarusian.farmer.model;
 
+import by.belarusian.farmer.enums.Color;
+import by.belarusian.farmer.enums.Type;
+
+import java.util.Objects;
+
 public class Lemon {
 
     private int weight;
+
+    private Color lemonColor;
+
+    private final Type TYPE = Type.FRUITS;
+
+    public Lemon(int weight, Color lemonColor) {
+        this.weight = weight;
+        this.lemonColor = lemonColor;
+    }
 
     public int getWeight() {
         return weight;
@@ -11,4 +25,43 @@ public class Lemon {
     public void setWeight(int weight) {
         this.weight = weight;
     }
+
+    public Color getLemonColor() {
+        return lemonColor;
+    }
+
+    public void setLemonColor(Color lemonColor) {
+        this.lemonColor = lemonColor;
+    }
+
+    public Type getTYPE() {
+        return TYPE;
+    }
+
+    @Override
+    public String toString() {
+        return "Lemon{" +
+                "weight=" + weight +
+                ", lemonColor=" + lemonColor +
+                ", TYPE=" + TYPE +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lemon lemon = (Lemon) o;
+        return weight == lemon.weight && lemonColor == lemon.lemonColor && TYPE == lemon.TYPE;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weight, lemonColor, TYPE);
+    }
+
+    public String getEnumFruitColor() {
+        return lemonColor.getRusName();
+    }
+
 }
