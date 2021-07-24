25f86401087056dac2bee0854a293f91f6a3abf4
гк <@adamant.local>
2021-07-24 19:49:55 +0300

    filter weight

--- src/by/belarusian/farmer/utils/HarvestUtil.java
+++ src/by/belarusian/farmer/utils/HarvestUtil.java
@@ -1,4 +1,16 @@
 package by.belarusian.farmer.utils;
 
+import by.belarusian.farmer.model.Harvest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class HarvestUtil {
+
+    public static <T extends Harvest> List<T> weightGreaterThen(Collection<? extends Harvest> collection, int weight){
+        List<?> list = collection.stream().filter(plod -> plod.getWeight() > weight).collect(Collectors.toList());
+        return (List<T>) list;
+    }
 }
