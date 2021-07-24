3424b23dba5d6810eaf64a96ebd2898b1e7c0292
FaQRed <sankovsky85@gmail.com>
2021-07-24 19:51:41 +0300

    Added filter by type in HarvestUtil

--- src/by/belarusian/farmer/utils/HarvestUtil.java
+++ src/by/belarusian/farmer/utils/HarvestUtil.java
@@ -1,6 +1,21 @@
 package by.belarusian.farmer.utils;
 
+import by.belarusian.farmer.enums.Type;
+import by.belarusian.farmer.model.Harvest;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HarvestUtil {
 
+    public List<Harvest> filter(List<Harvest> list, Type type) {
+        List<Harvest> filteredList = new ArrayList<>();
+        for (Harvest value : list) {
+            if (value.getType() == type) {
+                filteredList.add(value);
+            }
+        }
+        return filteredList;
+    }
 
 }
